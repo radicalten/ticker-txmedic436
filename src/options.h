a04327bcf347f74157cb6142a13d4d4ef8249cd4
radicalten <79894181+radicalten@users.noreply.github.com>
2025-09-28 09:25:45 -0500

    Add files via upload

---
+++ src/options.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <iostream>
+
+//Function used to print usage statement
+void PrintHelp();
+//void ProcessArgs(int *argc, char** argv[], int flag);
+
+#endif
---
+++ src/stock.cpp
@@ -0,0 +1,73 @@
+#include "stock.h"
+
+Stock::Stock(std::string symbol){
+	m_symbol = symbol;
+	m_url = GenerateURL(symbol);
+	GetWebsiteData();
+	m_current_price = ParsePrice(CURRENT_PRICE);
+	m_open_price = ParsePrice(OPEN_PRICE);
+	m_volume = ParsePrice(VOLUME);
+}
+
+std::string Stock::GetSymbol(){
+	std::transform(m_symbol.begin(), m_symbol.end(), m_symbol.begin(), ::toupper);
+	return m_symbol;
+}
+
+std::string Stock::GenerateURL(std::string symbol){
+	std::string url = "https://query1.finance.yahoo.com/v8/finance/chart/";
+	url.append(symbol + "?interval=1d");
+	return url;
+}
+
+size_t Stock::Callback(void* buffer, size_t size, size_t num, void* out){
+	const size_t totalBytes( size * num );
+	((std::string*)out)->append((char*)buffer, totalBytes);
+	return totalBytes;
+}
+
+
+//Fetch Website data
+bool Stock::GetWebsiteData(){
+	CURLcode res;
+	char error[CURL_ERROR_SIZE];
+	
+	CURL *curl = curl_easy_init();											//Initialize libcurl
+	curl_easy_setopt(curl, CURLOPT_URL, GenerateURL(m_symbol).c_str());		//Set the url
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);							//Set timeout to 10s
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Callback);				//Hook up data function
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &m_website_data);				//Hook up data container
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, &error);
+	error[0] = 0;
+	res = curl_easy_perform(curl);											//Fetch data from website
+	if(res != CURLE_OK){
+		std::cerr << "libcurl error: " << error << std::endl;
+		exit(EXIT_FAILURE);
+		return -1;
+	}
+	else{
+		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &m_http_std_res_code);	//Get response code
+		curl_easy_cleanup(curl);												//Release and cleanup libcurl
+		return 0;
+	}
+}
+
+float Stock::ParsePrice(const std::string type){
+	char end_delimiter = ',';
+
+	size_t start_pos = m_website_data.find(type) + type.length();
+	size_t end_pos = m_website_data.find(end_delimiter, start_pos);
+
+	std::string token = m_website_data.substr(start_pos, end_pos);
+	if(token[0] == '['){
+		//Remove first and last char's to remove brackets
+		token.erase(0, 1);
+		token.pop_back();
+	}
+	try {
+		return std::stod(token.substr(0, token.find(end_delimiter)));
+	}
+	catch(std::invalid_argument){
+		return -0.1;
+	}
+}
