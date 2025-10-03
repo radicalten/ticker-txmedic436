799b02ac3cade04e33b7f53d88d8061e5b12df5a
radicalten <79894181+radicalten@users.noreply.github.com>
2025-10-02 20:56:38 -0500

    Update main.c

--- wii4/main.c
+++ wii4/main.c
@@ -286,15 +286,6 @@ int main(int argc, char** argv) {
     // Wii video & network init
 #if defined(GEKKO) && defined(HW_RVL)
     wii_video_init();
-    // The original documentation for libwiisocket showed using multiple tries to init and get an ip.
-	// I don't think this is necessary, but I'm leaving it in just in case.
-	int socket_init_success = -1;
-	for (int attempts = 0;attempts < 3;attempts++) {
-		socket_init_success = wiisocket_init();
-		printf("attempt: %d wiisocket_init: %d\n", attempts, socket_init_success);
-		if (socket_init_success == 0)
-			break;
-
 		u32 ip = 0;
 	for (int attempts = 0; attempts < 3; attempts++) {
 		ip = gethostid();
