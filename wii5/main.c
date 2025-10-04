fbd49e0029b53eeb52718ef0ad4c7df1c5b8778c
radicalten <79894181+radicalten@users.noreply.github.com>
2025-10-03 20:02:53 -0500

    Update main.c

--- wii5/main.c
+++ wii5/main.c
@@ -54,14 +54,14 @@ const int num_tickers = sizeof(tickers) / sizeof(tickers[0]);
 
 // --- Color Definitions for Terminal ---
 #define KNRM  "\x1B[0m"  // Default attributes 
-#define KBLK  "\x1B[30m" // Black
+//#define KBLK  "\x1B[30m" // Black
 #define KRED  "\x1B[31m" // Red
 #define KGRN  "\x1B[32m" // Green
 #define KYEL  "\x1B[33m" // Yellow
-#define KBLU  "\x1B[34m" // Blue
-#define KMAG  "\x1B[35m" // Magenta
+//#define KBLU  "\x1B[34m" // Blue
+//#define KMAG  "\x1B[35m" // Magenta
 #define KCYN  "\x1B[36m" // Cyan
-#define KWHT  "\x1B[37m" // White
+//#define KWHT  "\x1B[37m" // White
 
 /* --- Regular Background Colors ---
 #define BGBLK "\x1B[40m" // Black
