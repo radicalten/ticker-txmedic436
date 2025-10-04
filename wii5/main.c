fd4072ba1cf2ed403ba4e9c760413bbbe99ccc85
radicalten <79894181+radicalten@users.noreply.github.com>
2025-10-03 19:39:42 -0500

    Update main.c

--- wii5/main.c
+++ wii5/main.c
@@ -87,6 +87,20 @@ int main(void) {
     // Register cleanup function to run on exit (e.g., Ctrl+C)
     atexit(cleanup_on_exit);
 
+       // Wii video & network init
+#if defined(GEKKO) && defined(HW_RVL)
+    wii_video_init();
+    // The original documentation for libwiisocket showed using multiple tries to init and get an ip.
+	// I don't think this is necessary, but I'm leaving it in just in case.
+	int socket_init_success = -1;
+	for (int attempts = 0;attempts < 3;attempts++) {
+		socket_init_success = wiisocket_init();
+		printf("attempt: %d wiisocket_init: %d\n", attempts, socket_init_success);
+		if (socket_init_success == 0)
+			break;
+	}
+#endif
+    
     // Initialize libcurl
     curl_global_init(CURL_GLOBAL_ALL);
 
