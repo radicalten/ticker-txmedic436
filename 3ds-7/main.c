6a79f20d18ca7ec301495d7e05c6ab9e3adeed27
radicalten <79894181+radicalten@users.noreply.github.com>
2025-10-07 22:54:38 -0500

    Update main.c

--- 3ds-7/main.c
+++ 3ds-7/main.c
@@ -692,7 +692,7 @@ void update_timestamp() {
 
 void update_status_line(int seconds_left) {
     int update_line = DATA_START_ROW + num_tickers + 1;
-    printf("\033[%d;1H\033[K%Updating in %2d s", update_line, seconds_left);
+    printf("\033[%d;1H\033[KUpdating in %2d s", update_line, seconds_left);
     fflush(stdout);
     #ifdef __3DS__
         svcSleepThread(1000000000LL);
