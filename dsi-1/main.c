ba43bb79ec9f74a832f9d5815407ae7d2f4a422c
radicalten <79894181+radicalten@users.noreply.github.com>
2025-10-19 17:22:25 -0500

    Update main.c

--- dsi-1/main.c
+++ dsi-1/main.c
@@ -594,7 +594,7 @@ void setup_dashboard_ui() {
     printf("\n"); // timestamp line
   
     // Headers (compact for DSi width) DSi = 24 rows x 32 columns
-    printf("%-4s|%6s|%4s|%4s|%4s\n", "Tkr", "Price", "%Chg", "MACD", "Sig");
+    printf("%-8s|%9s|%5s|%3s|%3s\n", "Tkr", "Price", "%Chg", "MAC", "Sig");
     printf("--------------------------------\n");
   
     // Placeholders
