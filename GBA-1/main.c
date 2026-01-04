e17e876fc5bad47969d750a71723cc3ec8f54bfc
radicalten <79894181+radicalten@users.noreply.github.com>
2026-01-03 20:02:40 -0600

    Update main.c

--- GBA-1/main.c
+++ GBA-1/main.c
@@ -20,6 +20,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 //-----------------------------------------------------------------------------
 // Data types
