5f820972bfb894183054d62cff0e1c2719db0e36
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-07 14:13:30 -0500

    Update nnue.c

--- chess3/nnue.c
+++ chess3/nnue.c
@@ -41,9 +41,9 @@
 #ifdef NNUE_EMBEDDED
 //#include "incbin.h"
 //INCBIN(Network, DefaultEvalFile);
-extern const char _binary_nn_37f18f62d772_nnue_start[];
-extern const char _binary_nn_37f18f62d772_nnue_end[];
-extern const size_t _binary_nn_37f18f62d772_nnue_size;
+extern const char _binary_nn_62ef826d1a6d_nnue_start[];
+extern const char _binary_nn_62ef826d1a6d_nnue_end[];
+extern const size_t _binary_nn_62ef826d1a6d_nnue_size;
 
 static inline const void *get_nnue_data(void) {
     return (const void *)_binary_nn_37f18f62d772_nnue_start;
