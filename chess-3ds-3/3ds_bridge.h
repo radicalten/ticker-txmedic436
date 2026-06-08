573cb6a23ed1325b19330b1eda004540e29e2b94
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-08 17:55:21 -0500

    Update 3ds_bridge.h

--- chess-3ds-3/3ds_bridge.h
+++ chess-3ds-3/3ds_bridge.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <pthread.h>
 
 // Struct for thread-safe circular communication queues
 typedef struct {
@@ -16,23 +17,24 @@ typedef struct {
 extern SafeQueue gui_to_engine;
 extern SafeQueue engine_to_gui;
 
-// Intercept POSIX calls inside uci.c
-ssize_t cfish_getline(char **lineptr, size_t *n, FILE *stream);
-int cfish_printf(const char *format, ...);
-int cfish_putchar(int c);
-int cfish_puts(const char *s);
+// Intercept Standard POSIX functions strictly for the Chess Engine
+#ifdef ENGINE_MODE
+  ssize_t cfish_getline(char **lineptr, size_t *n, FILE *stream);
+  int cfish_printf(const char *format, ...);
+  int cfish_putchar(int c);
+  int cfish_puts(const char *s);
 
-// Preprocessor macro injections
-#define stdin  ((FILE*)0x1)
-#define stdout ((FILE*)0x2)
-#define stderr ((FILE*)0x3)
+  #define stdin  ((FILE*)0x1)
+  #define stdout ((FILE*)0x2)
+  #define stderr ((FILE*)0x3)
 
-#define getline(lineptr, n, stream) cfish_getline(lineptr, n, stream)
-#define printf(...) cfish_printf(__VA_ARGS__)
-#define putchar(c) cfish_putchar(c)
-#define puts(s) cfish_puts(s)
-#define fflush(stream) ((void)0)
-#define flockfile(stream) ((void)0)
-#define funlockfile(stream) ((void)0)
+  #define getline(lineptr, n, stream) cfish_getline(lineptr, n, stream)
+  #define printf(...) cfish_printf(__VA_ARGS__)
+  #define putchar(c) cfish_putchar(c)
+  #define puts(s) cfish_puts(s)
+  #define fflush(stream) ((void)0)
+  #define flockfile(stream) ((void)0)
+  #define funlockfile(stream) ((void)0)
+#endif
 
 #endif // CTRU_3DS_BRIDGE_H
