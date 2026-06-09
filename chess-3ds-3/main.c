a9bbe9b75ec14b4718e3c69be478d508310eebab
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-09 16:14:52 -0500

    Update main.c

--- chess-3ds-3/main.c
+++ chess-3ds-3/main.c
@@ -264,8 +264,8 @@ void process_engine_output(char *line) {
         }
     } else if (engine_state == ENGINE_STATE_WAIT_READYOK) {
         if (strstr(line, "readyok") != NULL) {
-            // Memory Optimization: 16MB Hash is safe for 128MB RAM
-            sf_send_command("setoption name Hash value 16"); 
+            // Memory Optimization: More Hash = More speed 1HV = 1MB, 128MB 3DS
+            sf_send_command("setoption name Hash value 64"); 
             // Explicitly disable pondering to optimize battery and CPU usage
             sf_send_command("setoption name Ponder value false");
             
