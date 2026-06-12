6d87350c79a5e6dcb5747fd9ac71b191f95c1eda
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-11 20:38:36 -0500

    Update embedded_book.c

--- chess5/embedded_book.c
+++ chess5/embedded_book.c
@@ -1,11 +1,13 @@
+#ifdef USE_EMBEDDED_BOOK
+
 #define INCBIN_PREFIX embedded_
 #define INCBIN_STYLE INCBIN_STYLE_SNAKE
 #include "incbin.h"
 
-/* 
-  This registers book.bin into the binary.
-  It generates symbols matching our prefix/style:
-    - const unsigned char embedded_book_data[]
-    - const unsigned int embedded_book_size
-*/
+/* Embeds book.bin into raw machine code */
 INCBIN(book, "book.bin");
+
+#else
+// Prevent compiler warnings for empty translation unit when disabled
+typedef int make_iso_compiler_happy;
+#endif
