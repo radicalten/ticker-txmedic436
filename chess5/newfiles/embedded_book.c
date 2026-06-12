4421616db384f296df6426965bcfc3fdf1dc09b3
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-11 22:43:47 -0500

    Add files via upload

---
+++ chess5/newfiles/embedded_book.c
@@ -0,0 +1,13 @@
+#ifdef USE_EMBEDDED_BOOK
+
+#define INCBIN_PREFIX embedded_
+#define INCBIN_STYLE INCBIN_STYLE_SNAKE
+#include "incbin.h"
+
+/* Embeds book.bin into raw machine code */
+INCBIN(book, "Balsa3750.bin");
+
+#else
+// Prevent compiler warnings for empty translation unit when disabled
+typedef int make_iso_compiler_happy;
+#endif
---
+++ chess5/newfiles/embedded_book.h
@@ -0,0 +1,9 @@
+#ifndef EMBEDDED_BOOK_H
+#define EMBEDDED_BOOK_H
+
+#ifdef USE_EMBEDDED_BOOK
+extern const unsigned char embedded_book_data[];
+extern const unsigned int embedded_book_size;
+#endif
+
+#endif // EMBEDDED_BOOK_H
