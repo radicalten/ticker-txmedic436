cb5c4a49b163da8d2297918aa9abb4ca59c3917e
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-12 16:13:59 -0500

    Update embedded_book.c

--- chess-3ds-5/embedded_book.c
+++ chess-3ds-5/embedded_book.c
@@ -5,7 +5,7 @@
 #include "incbin.h"
 
 /* Embeds book.bin into raw machine code */
-INCBIN(book, "SSDF26.bin");
+INCBIN(book, "Best.bin");
 
 #else
 // Prevent compiler warnings for empty translation unit when disabled
