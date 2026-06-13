41e2373a2fb017ec2d53eea0bff2482c0a3b3b22
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-12 22:05:31 -0500

    Update embedded_book.h

--- chess-3ds-5/embedded_book.h
+++ chess-3ds-5/embedded_book.h
@@ -2,8 +2,25 @@
 #define EMBEDDED_BOOK_H
 
 #ifdef USE_EMBEDDED_BOOK
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const unsigned char embedded_book_data[];
+
+#if defined(__3DS__)
+/* 3DS Specific: Use linker symbol subtraction to prevent size evaluation crashes */
+extern const unsigned char embedded_book_end[];
+#define embedded_book_size ((unsigned int)(embedded_book_end - embedded_book_data))
+#else
+/* Non-3DS standard fallback */
 extern const unsigned int embedded_book_size;
 #endif
 
+#ifdef __cplusplus
+}
+#endif
+
+#endif // USE_EMBEDDED_BOOK
 #endif // EMBEDDED_BOOK_H
