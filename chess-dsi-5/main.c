f8a47f49e2b6db933700492e69c3d76ffe0be036
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-21 07:07:46 -0500

    Update main.c

--- chess-dsi-5/main.c
+++ chess-dsi-5/main.c
@@ -306,7 +306,7 @@ void process_engine_output(char *line) {
         }
     } else if (engine_state == ENGINE_STATE_WAIT_READYOK) {
         if (strstr(line, "readyok") != NULL) {
-            sf_send_command("setoption name Hash value 0"); 
+            sf_send_command("setoption name Hash value 1"); 
             sf_send_command("setoption name Ponder value false");
             sf_send_command("ucinewgame");
             engine_state = ENGINE_STATE_READY;
