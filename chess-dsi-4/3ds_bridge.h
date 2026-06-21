663feaf8d0830d657f76d9a514227b4ea6298afa
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-20 20:37:15 -0500

    Update 3ds_bridge.h

--- chess-dsi-4/3ds_bridge.h
+++ chess-dsi-4/3ds_bridge.h
@@ -11,7 +11,7 @@
 // On Nintendo DS/DSi, define 3DS compatibility wrappers
 #ifdef __NDS__
 #include <sys/time.h>      // Required for gettimeofday
-#include <calico/thread.h> // Calico preemptive threading engine
+#include <calico/system/thread.h> // Calico preemptive threading engine
 
 // Real thread-blocking LightLock utilizing Calico queues
 typedef struct {
