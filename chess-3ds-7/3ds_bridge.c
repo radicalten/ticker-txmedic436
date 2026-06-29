2633c8cb829d51b3245222eaac7392d7b4bb2af7
radicalten <79894181+radicalten@users.noreply.github.com>
2026-06-29 07:56:38 -0500

    Update 3ds_bridge.c

--- chess-3ds-7/3ds_bridge.c
+++ chess-3ds-7/3ds_bridge.c
@@ -4,8 +4,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-// Expanded to 64KB to easily handle heavy Stockfish PV search info outputs
-#define MSG_QUEUE_SIZE 65536
+// Expanded to 4KB to easily handle heavy Stockfish PV search info outputs
+#define MSG_QUEUE_SIZE 4096
 
 typedef struct {
     char data[MSG_QUEUE_SIZE];
